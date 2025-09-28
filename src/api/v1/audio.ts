f191efaf5f55dd08f8e26768afb54e3befdc5b32
significantnose <yanucevich.d@gmail.com>
2025-09-28 22:59:44 +0300

    feat(app): s3 storage for audio files introduced

--- src/api/v1/audio.ts
+++ src/api/v1/audio.ts
@@ -1,33 +1,43 @@
 import { Router } from 'express';
 import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import config from '@assets/config';
 
 const api = Router();
 
-
-const UPLOAD_DIR = '/home/nose/audio-uploads';
-if (!fs.existsSync(UPLOAD_DIR)) fs.mkdirSync(UPLOAD_DIR);
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, UPLOAD_DIR);
+const s3 = new S3Client({
+  endpoint: config.audioFileStorage.url,
+  region: config.audioFileStorage.region,
+  credentials: {
+    accessKeyId: config.audioFileStorage.accessKeyId,
+    secretAccessKey: config.audioFileStorage.accessKeySecret
   },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  },
+  forcePathStyle: true,
 });
+const upload = multer({ storage: multer.memoryStorage() });
 
-const upload = multer({ storage });
-
-api.post('/', upload.single('file'), (req, res) => {
+api.post('/', upload.single('file'), async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: 'No file uploaded' });
   }
 
-  const fileUri = `${req.file.filename}`;
-  return res.status(201).json({ uri: fileUri });
+  const key = `${Date.now()}-${req.file.originalname}`;
+  try {
+    await s3.send(
+      new PutObjectCommand({
+        Bucket: config.audioFileStorage.bucketName,
+        Key: key,
+        Body: req.file.buffer,
+        ContentType: req.file.mimetype,
+      })
+    );
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Upload failed" });
+  }
+
+  return res.status(201).json({ uri: key });
 });
 
 export default api;
--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,11 +1,25 @@
 import "dotenv/config";
 
-interface Config {
+interface AppConfig {
   port: number;
+  audioFileStorage: {
+    url: string;
+    region: string;
+    accessKeyId: string;
+    accessKeySecret: string;
+    bucketName: string;
+  }
 }
 
-const config: Config = {
+const config: AppConfig = {
   port: parseInt(process.env.PORT || "3000", 10),
+  audioFileStorage: {
+    url: process.env.AUDIO_STORAGE_URL || "http://localhost:9000",
+    region: process.env.AUDIO_STORAGE_REGION || "us-east-1",
+    accessKeyId: process.env.AUDIO_STORAGE_ACCESS_KEY_ID || "admin",
+    accessKeySecret: process.env.AUDIO_STORAGE_ACCESS_KEY_SECRET || "supersecretpassword",
+    bucketName: process.env.AUDIO_STORAGE_BUCKET_NAME || "audio-uploads"
+  },
 };
 
 export default config;
